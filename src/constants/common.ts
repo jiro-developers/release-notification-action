e871c867ac7f68db1562e816e9027855cef73c97
FrontLeejonghun <leejonghun32425@gmail.com>
2025-01-05 20:46:37 +0900

    feat: 기본적으로 필요한 REQUIRED 를 상수관리를 합니다.
    github action input 에서 필수값으로 받는것들을 상수화 처리합니다.

---
+++ src/constants/common.ts
@@ -0,0 +1,10 @@
+/**
+ * 액션에서 필수적으로 사용되는 입력되어야 할 키 값입니다.
+ * **/
+const ACTION_REQUIRED_INPUT_KEY = [
+  'token', // GitHub Token
+  'extractionPoint', // 본문을 추출할 기점 해당 기점 문자열 ex) "## 리뷰 요약 정보"
+  'slackWebHookURL', // Slack WebHook URL
+] as const;
+
+export { ACTION_REQUIRED_INPUT_KEY };

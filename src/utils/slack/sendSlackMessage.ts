1951b2014dd6d4bb26df434e837d695cd49dcab7
FrontLeejonghun <leejonghun32425@gmail.com>
2025-01-05 20:44:17 +0900

    feat: 슬랙 관련 유틸을 모듈화 및 추가합니다.
    buildSlackMessage
    - 슬랙 메시지를 포멧에 맞게 만들어주는 유틸 함수 입니다.
    
    sendSlackMessage
    - webhook url을 통해 슬랙 메시지를 전달합니다.

---
+++ src/utils/slack/sendSlackMessage.ts
@@ -0,0 +1,25 @@
+import * as core from '@actions/core';
+
+import type { SlackMessagePayload } from './buildSlackMessage';
+
+interface SendSlackMessageProps {
+  webhookUrl: string;
+  payload: SlackMessagePayload;
+}
+const sendSlackMessage = async ({ webhookUrl, payload }: SendSlackMessageProps) => {
+  core.info(`Sending a message to Slack...${webhookUrl}`);
+  const response = await fetch(webhookUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    const errorMessage = `status: ${response.status}`;
+    throw new Error(errorMessage);
+  }
+};
+
+export { sendSlackMessage };

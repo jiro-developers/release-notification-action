bd121655b43ffe9073ba845acaa5e65a2d18168d
FrontLeejonghun <leejonghun32425@gmail.com>
2025-02-01 00:46:13 +0900

    refactor: extractionStartPoint과extractionEndPoint 에 대한 값을 명시적으로 수정합니다.

--- src/utils/extractSection.ts
+++ src/utils/extractSection.ts
@@ -2,8 +2,8 @@
  * @name extractSection
  * @description 정규식을 사용하여 특정 섹션을 추출하는 함수
  * @param input - 추출할 섹션을 포함한 전체 문자열
- * @param extractionStartPoint - 본문을 추출할 기점 해당 기점 문자열 ex) "## 리뷰 요약 정보"
- * @param extractionEndPoint - 본문을 추출할 종점 해당 종점 문자열 ex) "## 리뷰 요약 정보"
+ * @param extractionStartPoint - 본문을 추출할 기점 해당 기점 문자열 ex) "## == 릴리즈 내용 시작 =="
+ * @param extractionEndPoint - 본문을 추출할 종점 해당 종점 문자열 ex) "## == 릴리즈 내용 종료 =="
  * @returns 추출된 섹션 문자열 또는 null (섹션을 찾지 못한 경우)
  * **/
 const extractSection = (input: string, extractionStartPoint: string, extractionEndPoint?: string): string | null => {
@@ -16,11 +16,12 @@ const extractSection = (input: string, extractionStartPoint: string, extractionE
     return null;
   }
 
-  const startIndex = startMatch.index;
+  // 시작점을 제외한 문자열을 반환하기 위해 시작점의 인덱스 계산
+  const startIndex = startMatch.index + startMatch[0].length;
 
   // 종료점이 제공되지 않은 경우, 시작점부터 문자열 끝까지 반환
   if (!extractionEndPoint) {
-    return input.slice(startIndex);
+    return input.slice(startIndex).trim();
   }
 
   // 종료점을 찾기 위해 정규식 사용
@@ -34,7 +35,7 @@ const extractSection = (input: string, extractionStartPoint: string, extractionE
 
   const endIndex = startIndex + endMatch.index;
 
-  return input.slice(startIndex, endIndex);
+  return input.slice(startIndex, endIndex).trim();
 };
 
 export { extractSection };

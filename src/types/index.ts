d02bfee6110f615aaa20efe6d5cd1ccb1398aa5f
FrontLeejonghun <leejonghun32425@gmail.com>
2025-02-13 10:43:46 +0900

    fix: deploy status 를 추가합니다.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 /**
  * @see https://docs.github.com/en/webhooks/webhook-events-and-payloads#deployment_status -> deployment_status -> state
+ * @see   error, failure, inactive, in_progress, queued, pending, success -> deployment_status -> state
  **/
-type GithubDeploymentStatusState = 'pending' | 'success' | 'failure' | 'error';
+type GithubDeploymentStatusState = 'pending' | 'success' | 'failure' | 'error' | 'inactive' | 'in_progress' | 'queued';
 
 export type { GithubDeploymentStatusState };

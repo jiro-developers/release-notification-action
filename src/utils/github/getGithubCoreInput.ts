7e9b1437aca9c4202b02b488509376e95e17cd2e
FrontLeejonghun <leejonghun32425@gmail.com>
2025-02-18 13:50:01 +0900

    refactor: core input 유효성 검증을 get 단계에서 처리

--- src/utils/github/getGithubCoreInput.ts
+++ src/utils/github/getGithubCoreInput.ts
@@ -1,14 +1,29 @@
 import * as core from '@actions/core';
 
 import { ACTION_INPUT_KEY_LIST } from '@/constants/common';
-import { coreLogger } from '@/utils/github/coreLogger';
+import type { ActionInputKey, OptionalActionInputKey, RequiredActionInputKey } from '@/types';
+import { logger } from '@/utils/github/logger';
 
 const getGithubCoreInput = () => {
-  const result = Object.fromEntries(ACTION_INPUT_KEY_LIST.map((key) => [key, core.getInput(key)]));
+  const result = Object.fromEntries(ACTION_INPUT_KEY_LIST.map((key) => [key, core.getInput(key)])) as Record<
+    ActionInputKey,
+    string
+  >;
 
-  coreLogger.info(result);
+  logger.info(result);
 
-  return result;
+  const missingInputKeys = ACTION_INPUT_KEY_LIST.filter((key) => !result[key]);
+
+  if (missingInputKeys.length) {
+    logger.error(`Missing required inputs: ${missingInputKeys.join(', ')}`);
+    return null;
+  }
+
+  return result as {
+    [key in RequiredActionInputKey]: string;
+  } & {
+    [key in OptionalActionInputKey]?: string;
+  };
 };
 
 export { getGithubCoreInput };
